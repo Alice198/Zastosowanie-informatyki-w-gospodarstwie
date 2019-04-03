f795237c6876cdb4ef3a56ad2868106dfb3e46cd
Ala <tomczakala@wp.pl>
2019-04-03 19:47:59 +0200

    Change link name

--- main/urls.py
+++ main/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.base, name='base'),
-    path('home/', views.home, name='home'),
-    path('login/', views.login, name='login'),
-    path('registration/', views.registration, name='registration'),
-    path('user_account/', views.user_account, name='user_account'),
-    path('edit_user/', views.edit_user, name='edit_user'),
-    path('change_password/', views.change_password, name='change_password'),
+    path('strona_glowna/', views.home, name='Home'),
+    path('logowanie/', views.login, name='Login'),
+    path('rejesracja/', views.registration, name='Registration'),
+    path('moje_konto/', views.user_account, name='User account'),
+    path('edytuj_dane/', views.edit_user, name='User edit'),
+    path('zmien_haslo/', views.change_password, name='Change password'),
 
 ]

0ea9786dfbee160eecddf420f1a50725e83e9676
Sebastian Rys <sebastian.rys.95@gmail.com>
2019-06-04 00:40:57 +0200

    some fix

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.models.order import Order
+from main.models.reviews import Reviews
+
+
+class ListedOrders(admin.ModelAdmin):
+    list_filter = ('order_date', 'costs', 'is_finished', 'user')
+    list_display = ('is_finished', 'user', 'died', 'coffin', 'flowers', 'music', 'order_date', 'costs')
+
+
+class ListedReviews(admin.ModelAdmin):
+    list_filter = ('rating', 'user', 'date_added')
+
+
+admin.site.register(Order, ListedOrders)
+admin.site.register(Reviews, ListedReviews)
